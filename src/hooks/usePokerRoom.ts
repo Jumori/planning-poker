782a9dba8bfa1089e6fa7de41221b02ea033e19f
Juliana Morikoshi <julianamorikoshi@gmail.com>
2022-06-08 19:19:58 -0300

    feat: usePokerRoom room players

--- src/hooks/usePokerRoom.ts
+++ src/hooks/usePokerRoom.ts
@@ -5,9 +5,15 @@ export const votingSystems = {
   fibonacci: ['1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '?']
 }
 
+type PlayerData = {
+  id: string
+  name: string
+}
+
 export type UsePokerRoomType = {
   title: string
   votingSystem: keyof typeof votingSystems | ''
+  players: PlayerData[]
 }
 
 export const usePokerRoom = (pokerRoomId?: string): UsePokerRoomType => {
@@ -15,16 +21,33 @@ export const usePokerRoom = (pokerRoomId?: string): UsePokerRoomType => {
   const [votingSystem, setVotingSystem] = useState<
     keyof typeof votingSystems | ''
   >('')
+  const [players, setPlayers] = useState<PlayerData[]>([])
 
   useEffect(() => {
     const pokerRoomRef = ref(database, `pokerRooms/${pokerRoomId}`)
     onValue(pokerRoomRef, room => {
       const databaseRoom = room.val()
+      const databaseRoomPlayers = databaseRoom.players as {
+        [key: string]: {
+          name: string
+        }
+      }
+
+      if (databaseRoomPlayers) {
+        const parsedPlayers = Object.entries(databaseRoomPlayers).map(
+          ([playerId, playerName]: [string, { name: string }]) => ({
+            id: playerId,
+            name: playerName.name
+          })
+        )
+
+        setPlayers(parsedPlayers)
+      }
 
       setTitle(databaseRoom.title)
       setVotingSystem(databaseRoom.votingSystem)
     })
   }, [])
 
-  return { title, votingSystem }
+  return { title, votingSystem, players }
 }
